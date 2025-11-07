eaf66e3b965e986100d70bb1e3dee0430c206d11
phuonganh <phuonganhnguyenomg@gmail.com>
2025-11-07 08:37:51 +0100

    done creating header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,20 +7,25 @@ import { AiOutlineSearch } from 'react-icons/ai'
 function Header() {
   const pathName=usePathname()
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
-      <Input placeholder='Search blog...'/>
-      <Button>
-        <AiOutlineSearch/>
-      </Button>
+    <nav className='flex justify-between items-center shadow p-3.5 border-blue-400'>
+      <h2>PA Blog</h2>
+      <div className='flex gap-2.5'>
+        <Link className={pathName==="/"? "text-blue-600":""} href="/">Home</Link>
+        <Link className={pathName==="/about" ? "text-blue-600":""} href="/about">About</Link>
+        <Link className={pathName==="/projects" ? "text-blue-600":""} href="/projects">Projects</Link>
+      </div>
+      
+      <div className='flex gap-1.5'>
+        <Input placeholder='Search blog...'/>
+        <Button>
+          <AiOutlineSearch/>
+        </Button>
+      </div>
+
       <Link href="/sign-in">
-        <Button>Sign in</Button>
+        <Button  variant='outline'>Sign in</Button>
       </Link>
-      <Link href="/sign-up">
-        <Button>Sign up</Button>
-      </Link>
+      
 
 
     </nav>
